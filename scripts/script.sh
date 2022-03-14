0288cdb21d45db8a3595264c2f9bde2a9da54699
dekauliya <deka108@gmail.com>
2022-03-13 20:18:25 -0400

    update script to write log to file

--- scripts/script.sh
+++ scripts/script.sh
@@ -2,4 +2,6 @@
 
 # require DB_TOKEN as env variable
 # require DB_URL
-cd refactoring && uvicorn server:app --reload --port 9999 --host 0.0.0.0
+mkdir -p /tmp/log
+touch /tmp/log/refactoring.log
+cd refactoring && uvicorn server:app --reload --port 9999 --host 0.0.0.0 2>&1 | tee /tmp/log/refactoring.log
